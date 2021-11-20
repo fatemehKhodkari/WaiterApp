9f384846e4886e9183a409262a22b173b9436b0d
fhkhd <fhkhdkri.yahoo.com>
2021-11-20 22:15:49 +0330

    add background pic to first activity & make gravity

--- app/src/main/java/com/example/waiterapp/MainActivity.java
+++ app/src/main/java/com/example/waiterapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.waiterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView= findViewById(R.id.img_back);
     }
 }
