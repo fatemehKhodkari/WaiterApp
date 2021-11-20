55e66b4a8f0e06af33a0d27adcb80578bab15623
fhkhd <fhkhdkri.yahoo.com>
2021-11-21 01:37:25 +0330

    make fade blur add waiter.json

--- app/src/main/java/com/example/waiterapp/MainActivity.java
+++ app/src/main/java/com/example/waiterapp/MainActivity.java
@@ -3,16 +3,43 @@ package com.example.waiterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    ImageView imageView1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView= findViewById(R.id.img_back);
+        imageView1= findViewById(R.id.img_back_blur);
+
+        final Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(1500);
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {imageView.setVisibility(View.GONE);}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+        imageView.startAnimation(fadeOut);
+
+
     }
 }
