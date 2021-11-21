4a1e2045ed010ef6d38c308b18de7928e258c009
fhkhd <fhkhdkri.yahoo.com>
2021-11-21 03:41:43 +0330

    set radius to editext

---
+++ app/src/main/java/com/example/waiterapp/activity/homepage/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.waiterapp.activity.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.waiterapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
