78e437bd28ad0261b710d83ec033aedd9bee2550
fhkhd <fhkhdkri.yahoo.com>
2021-11-25 21:41:30 +0330

    hide action bar

--- app/src/main/java/com/example/waiterapp/activity/customer/AddEditCostomerActivity.java
+++ app/src/main/java/com/example/waiterapp/activity/customer/AddEditCostomerActivity.java
@@ -3,6 +3,8 @@ package com.example.waiterapp.activity.customer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.waiterapp.R;
 
@@ -11,6 +13,12 @@ public class AddEditCostomerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideActionBar();
         setContentView(R.layout.activity_add_edit_costomer);
+    }
+    void hideActionBar(){
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
