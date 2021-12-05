3dde7b4401c91e28aa158c378093b11f6af34352
fhkhd <fhkhdkri.yahoo.com>
2021-12-05 15:36:06 +0330

    set savabtt and cancle bttn to add edit product activity

--- app/src/main/java/com/example/waiterapp/activity/product/AddEditProductActivity.java
+++ app/src/main/java/com/example/waiterapp/activity/product/AddEditProductActivity.java
@@ -4,19 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.waiterapp.R;
 import com.example.waiterapp.database.DatabaseHelper;
 import com.example.waiterapp.database.dao.GroupingDao;
 import com.example.waiterapp.database.dao.ProductDao;
+import com.example.waiterapp.design.NumberTextWatcherForThousand;
+import com.example.waiterapp.model.Customer;
 import com.example.waiterapp.model.Grouping;
 import com.example.waiterapp.model.Product;
+import com.google.gson.Gson;
 import com.r0adkll.slidr.Slidr;
 import com.r0adkll.slidr.model.SlidrInterface;
 
@@ -32,6 +39,7 @@ public class AddEditProductActivity extends AppCompatActivity {
     ProductDao productDao;
     Product product;
     GroupingDao groupingDao;
+    String product_name , product_grouping_name , product_price ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +50,11 @@ public class AddEditProductActivity extends AppCompatActivity {
         slidrInterface = Slidr.attach(this);
         init();
         call_db();
+        check_db();
+        thousandNumbersEdt();
         animateOb();
+        save_bttn();
+        cancle_bttn();
 
     }
     void hideActionBar(){
@@ -52,11 +64,16 @@ public class AddEditProductActivity extends AppCompatActivity {
     }
 
     void init(){
-        save_product = findViewById(R.id.save_customer);
-        cancle_tv = findViewById(R.id.cancel_customer);
-        product_name_edt = findViewById(R.id.get_customer_name);
+        save_product = findViewById(R.id.save_product);
+        cancle_tv = findViewById(R.id.cancel_product);
+        product_name_edt = findViewById(R.id.get_product_name);
+        product_price_edt = findViewById(R.id.get_product_price);
+        product_grouping_name_actv = findViewById(R.id.get_product_grouping_name);
         product_anim_feilds = findViewById(R.id.product_info_feilds);
         product_desing_anim = findViewById(R.id.add_edit_product_design);
+    }
+    void thousandNumbersEdt(){
+        product_price_edt.addTextChangedListener(new NumberTextWatcherForThousand(product_price_edt));
     }
     void call_db(){
         databaseHelper = DatabaseHelper.getInstance(getApplicationContext());
@@ -70,5 +87,53 @@ public class AddEditProductActivity extends AppCompatActivity {
         product_anim_feilds.setTranslationX(+200f);
         product_anim_feilds.animate().translationXBy(-200f).setDuration(200);
     }
+    void check_db(){
+        if (getIntent().getExtras() != null){
+            String getNameProduct = getIntent().getStringExtra("product");
+            product = new Gson().fromJson(getNameProduct,Product.class);
+            product_name_edt.setText(product.name_product);
+            product_grouping_name_actv.setText(product.category);
+            product_price_edt.setText(product.price);
+        }
+    }
 
+    void save_bttn(){
+        save_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                product_name = product_name_edt.getText().toString();
+                product_grouping_name = product_grouping_name_actv.getText().toString();
+                product_price = product_price_edt.getText().toString();
+
+                if(product == null){
+                    if(TextUtils.isEmpty(product_name) || TextUtils.isEmpty(product_grouping_name) || TextUtils.isEmpty(product_price)){
+                        Toast.makeText(getApplicationContext(),"تمام فیلد هارا پر کنید!",Toast.LENGTH_SHORT).show();
+                    }else{
+                        productDao.insertProduct(new Product(product_name,product_grouping_name,product_price));
+                        Toast.makeText(getApplicationContext(),"با موفقیت به لیست اضافه شد",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }else {
+                    product.name_product = product_name;
+                    product.category = product_grouping_name;
+                    product.price = product_price;
+
+                    Log.e("qqqq","onClick: update product =" + product.id);
+                    productDao.updateProduct(product);
+                    Toast.makeText(getApplicationContext(),"تغییرات با موفقیت اعمال شد!" , Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        });
+    }
+
+    void cancle_bttn(){
+        cancle_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
