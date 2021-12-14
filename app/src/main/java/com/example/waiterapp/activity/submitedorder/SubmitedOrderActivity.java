1dc5e6461a99bf506d14a7f1c3567e149d59110e
fhkhd <fhkhdkri.yahoo.com>
2021-12-14 22:17:04 +0330

    add submited activity & create submited ordering info card & toolbar

---
+++ app/src/main/java/com/example/waiterapp/activity/submitedorder/SubmitedOrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.waiterapp.activity.submitedorder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.waiterapp.R;
+
+public class SubmitedOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submited_order);
+    }
+}
