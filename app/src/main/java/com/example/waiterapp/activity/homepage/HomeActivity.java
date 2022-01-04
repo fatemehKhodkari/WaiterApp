db0316f28ac63862be8f3c6e9c45c76ca4fa0382
fhkhd <fhkhdkri.yahoo.com>
2022-01-04 10:16:59 +0330

    set bar chart

--- app/src/main/java/com/example/waiterapp/activity/homepage/HomeActivity.java
+++ app/src/main/java/com/example/waiterapp/activity/homepage/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.waiterapp.activity.homepage;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,17 +23,20 @@ import com.example.waiterapp.database.dao.GroupingDao;
 import com.example.waiterapp.database.dao.ProductDao;
 import com.example.waiterapp.database.dao.SubmitOrderDao;
 import com.example.waiterapp.helper.App;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
     private CardView cardViewproduct,cardViewcustomer , cardViewgrouping , cardViewSubmittedOrdering;
     private ImageView add_order;
     private LinearLayout copy , share , upload, download , delete;
-    private GraphView graph;
+    private BarChart graph;
     private TextView num_product , num_customer , num_ordering , num_category ;
     private DatabaseHelper databaseHelper;
     private ProductDao productDao;
@@ -67,67 +71,37 @@ public class HomeActivity extends AppCompatActivity {
         cardViewgrouping=findViewById(R.id.grouping);
         cardViewSubmittedOrdering = findViewById(R.id.submitted_orders);
         add_order = findViewById(R.id.add_order_ic);
-        graph = (GraphView) findViewById(R.id.graf);
+        graph = (BarChart) findViewById(R.id.graf);
         num_product = findViewById(R.id.number_product);
         num_category = findViewById(R.id.number_category);
         num_ordering = findViewById(R.id.number_orderings);
         num_customer = findViewById(R.id.number_customer);
     }
 
-    void graph(){
-        LineGraphSeries<DataPoint> bgseries = new LineGraphSeries<>(new DataPoint[]{
+    private void graph(){
+        BarData barData;
+        BarDataSet barDataSet;
+        ArrayList barEntries;
 
-                new DataPoint(0, 1000),
-                new DataPoint(1, 8000),
-                new DataPoint(2, 3500),
-                new DataPoint(3, 24000),
-                new DataPoint(4, 60000),
-                new DataPoint(5, 1300),
-                new DataPoint(6, 5300),
-                new DataPoint(7, 3500),
-                new DataPoint(8, 80000),
-                new DataPoint(9, 3500),
-                new DataPoint(10, 1300),
-                new DataPoint(11, 5300),
-                new DataPoint(12, 80000),
-                new DataPoint(13, 3500),
-                new DataPoint(14, 60000),
-                new DataPoint(15, 5300),
-                new DataPoint(16, 8000),
-                new DataPoint(17, 1000),
-                new DataPoint(18, 5300),
-                new DataPoint(19, 60000),
-                new DataPoint(20, 5300),
-                new DataPoint(21, 1300),
-                new DataPoint(22, 60000),
-                new DataPoint(23, 3500),
-                new DataPoint(24, 1300),
-                new DataPoint(25, 1000),
-                new DataPoint(26, 8000),
-                new DataPoint(27, 5300),
-                new DataPoint(28, 1300),
-                new DataPoint(29, 60000),
-                new DataPoint(30, 5300),
-                new DataPoint(31, 80000),
-        });
+        barEntries = new ArrayList<>();
 
-        graph.addSeries(bgseries);
-//        graph.setBackgroundColor(getResources().getColor(R.color.brownopa));
-        graph.getViewport().setScalable(true);
-        graph.getViewport().setScrollable(true);
-//        graph.setTitle("نمودار رشد سرمایه");
-        graph.setTitleColor(getResources().getColor(R.color.browwn));
-//        bgseries.setTitle("foo");
-        bgseries.setThickness(5);
-        bgseries.setDrawBackground(true);
-        bgseries.setColor(getResources().getColor(R.color.browwn));
-//        bgseries.setBackgroundColor(Color.rgb(248,243,247));
-        bgseries.setBackgroundColor(getResources().getColor(R.color.whitediff));
-        bgseries.setAnimated(true);
-        bgseries.setColor(R.color.browwn);
-//        graph.getLegendRenderer().setVisible(true);
-        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        barEntries.add(new BarEntry(1f, 0.5f));
+        barEntries.add(new BarEntry(2f, 1.8f));
+        barEntries.add(new BarEntry(3f, 1));
+        barEntries.add(new BarEntry(4f, 3));
+        barEntries.add(new BarEntry(5f, 1));
+        barEntries.add(new BarEntry(6f, 4));
+        barEntries.add(new BarEntry(7f, 3));
+
+        barDataSet = new BarDataSet(barEntries, "");
+        barData = new BarData(barDataSet);
+        graph.setData(barData);
+        barDataSet.setColors(ColorTemplate.PASTEL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(18f);
+
     }
+
 
     void on_click_cards(){
         cardViewproduct.setOnClickListener(new View.OnClickListener() {
@@ -164,6 +138,7 @@ public class HomeActivity extends AppCompatActivity {
                 overridePendingTransition(android.R.anim.fade_in , android.R.anim.fade_out);
             }
         });
+
 
     }
 
